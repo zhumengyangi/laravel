a5526d2b6403f1cceeafa38a7f121b6bfef5dce0
zmy <1784311404@qq.com>
2019-04-21 20:26:08 +0800

    test2

--- config/oss.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    return [
-        'accessKeyId'=>'LTAI3BTrG0ZK82Hv',//访问阿里云access权限的账号id
-        'accessKeySecret'=>'1ivNeImQ5lwWF639Zsy3l2R7QHdYxU',//访问阿里云access权限的账号密钥
-        'endpoint'=>'oss-cn-beijing.aliyuncs.com',//权限的节点信息
-        'bucket'=>'zhumengyang',//存储空间的名称
-    ];
