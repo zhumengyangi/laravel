8f5058b62b58d3dbe35056f63420e1d36f5eb162
zmy <1784311404@qq.com>
2019-05-06 16:25:34 +0800

    DB

---
+++ database/migrations/2019_05_06_162217_user_table_add_openid.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UserTableAddOpenid extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jy_user', function (Blueprint $table) {
+            $table->string('open_id',60)->default('')->after('id')->comment('第三方登录的openid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jy_user', function (Blueprint $table) {
+            //
+        });
+    }
+}
